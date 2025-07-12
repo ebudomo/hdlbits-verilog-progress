10b09291c828f5d1f0848ae5fdc6acffcf381c5c
Eduardo B. <ebudomo@torontomu.ca>
2025-07-11 20:38:15 -0400

    Added modules-hierarchy from other PC.

---
+++ verilog-language/modules-hierarchy/module_instance.v
@@ -0,0 +1,26 @@
+module module_instance(
+	input a,
+	input b,
+	output out1,
+	output out2
+);
+	//instantiating mod_a and connecting ports by name
+	mod_a instance_1 (
+		.in1(a),
+		.in2(b),
+		.out(out1)
+	);
+	
+	//connecting ports by position
+	mod_a instance_2 ( a, b, out2);
+
+endmodule
+
+module mod_a(
+	input in1,
+	input in2,
+	output out 
+);
+	//module body
+
+endmodule 
