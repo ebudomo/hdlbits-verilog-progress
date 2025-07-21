251b75d5b062a3bb3d9cd26f54b2eae145cc7870
ebudomo <ebudomo@torontomu.ca>
2025-07-21 14:20:39 -0400

    Optimize shift4, fix always sensitivity list to Verilog-2001 standard syntax

--- circuits/sequential-logic/shift-registers/shift4.v
+++ circuits/sequential-logic/shift-registers/shift4.v
@@ -8,17 +8,14 @@ module shift4(
 );
 	// 4-bit right shift register with async reset
 	// load input has higher priority
-	always @(posedge clk or posedge areset) begin
+	always @(posedge clk, posedge areset) begin
 		if (areset)
 			q <= 4'b0;
 		else if (load)
 			q <= data;
-		else if (ena) begin
-			q[3] <= 1'b0;
-			q[2] <= q[3];
-			q[1] <= q[2];
-			q[0] <= q[1];
-		end
+		else if (ena) 
+			q <= q[3:1]; // top 3 bits become lower 3 bits
+			// vector q will be zero-extended
 	end
 	
 endmodule
