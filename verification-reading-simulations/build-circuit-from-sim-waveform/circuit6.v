c57e05d3ee54228eb97be253a733baeebb4aace4
Eduardo B. <ebudomo@torontomu.ca>
2025-08-31 18:04:51 -0400

    Update circuit6 default case statement to correct Verilog implementation of don't care bits in vector

--- verification-reading-simulations/build-circuit-from-sim-waveform/circuit6.v
+++ verification-reading-simulations/build-circuit-from-sim-waveform/circuit6.v
@@ -6,15 +6,15 @@ module circuit6 (
 	reg [15:0] x;
 	always @(*) begin
 		case (a)
-			3'd0: x = 0;
-			3'd1: x = 0;
-			3'd2: x = 0;
-			3'd3: x = 0;
-			3'd4: x = 0;
-			3'd5: x = 0;
-			3'd6: x = 0;
-			3'd7: x = 0;
-			default: x = 16'dx;
+			3'd0: x = 16'h1232;
+			3'd1: x = 16'haee0;
+			3'd2: x = 16'h27d4;
+			3'd3: x = 16'h5a0e;
+			3'd4: x = 16'h2066;
+			3'd5: x = 16'h64ce;
+			3'd6: x = 16'hc526;
+			3'd7: x = 16'h2f19;
+			default: x = 16'bx;
 		endcase
 	end
 	
